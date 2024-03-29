99616663757588be1e541994477249382743bc5c
AbdulhameedAjibola <105476350+AbdulhameedAjibola@users.noreply.github.com>
2024-03-29 23:24:21 +0400

    Create lessons.js

---
+++ lessons.js
@@ -0,0 +1,10 @@
+let lessons = [
+    {
+        "id": 1001,
+        "image": "images/Tennis.jpg",
+        "subject": "Tennis 101",
+        "price": 50.00,
+        "location": "Sports Centre GK17",
+        "description": "This is an extra lesson session for students who wish to improve their Table tennis skills",
+        "availability": 10
+    },
